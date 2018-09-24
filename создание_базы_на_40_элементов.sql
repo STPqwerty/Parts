01a565697466891231ede603e26ab6aaa0e8c5ea
Alex <aleksandrstp1>
2018-09-25 01:25:01 +0300

    added sqls

---
+++ создание_базы_на_40_элементов.sql
@@ -0,0 +1,44 @@
+INSERT INTO parts ( Name, Requared, Count) values('Системы охлаждения компьютера', true, 2);
+INSERT INTO parts ( Name, Requared, Count) values('Системы охлаждения процессора', true, 2);
+INSERT INTO parts ( Name, Requared, Count) values( 'Системы охлаждения корпуса', true, 0);
+INSERT INTO parts ( Name, Requared, Count) values( 'Модули памяти', true, 2);
+INSERT INTO parts ( Name, Requared, Count) values( 'блок питания ', true, 5);
+INSERT INTO parts ( Name, Requared, Count) values( 'процессор', true, 2);
+INSERT INTO parts ( Name, Requared, Count) values( 'подсветка корпуса', false, 10);
+INSERT INTO parts ( Name, Requared, Count) values( 'звуковая карта', true, 4);
+INSERT INTO parts ( Name, Requared, Count) values( 'кард ридер', false, 4);
+INSERT INTO parts ( Name, Requared, Count) values( 'корпус ', true, 2);
+INSERT INTO parts ( Name, Requared, Count) values('Видеокарты', true, 2);
+INSERT INTO parts ( Name, Requared, Count) values('Оптические приводы', true, 2);
+INSERT INTO parts ( Name, Requared, Count) values( 'ТВ-тюнеры и видеомонтаж', true, 0);
+INSERT INTO parts ( Name, Requared, Count) values( 'Аксессуары для устройств охлаждения', true, 2);
+INSERT INTO parts ( Name, Requared, Count) values( 'Термопаста ', true, 5);
+INSERT INTO parts ( Name, Requared, Count) values( 'Сетевые адаптеры', true, 2);
+INSERT INTO parts ( Name, Requared, Count) values( 'подсветка корпуса', false, 10);
+INSERT INTO parts ( Name, Requared, Count) values( 'звуковая карта', true, 4);
+INSERT INTO parts ( Name, Requared, Count) values( 'кард ридер', false, 4);
+INSERT INTO parts ( Name, Requared, Count) values( 'корпус ', true, 2);
+INSERT INTO parts ( Name, Requared, Count) values('Системы охлаждения компьютера', true, 2);
+INSERT INTO parts ( Name, Requared, Count) values('Системы охлаждения процессора', true, 2);
+INSERT INTO parts ( Name, Requared, Count) values( 'Системы охлаждения корпуса', true, 0);
+INSERT INTO parts ( Name, Requared, Count) values( 'Модули памяти', true, 2);
+INSERT INTO parts ( Name, Requared, Count) values( 'блок питания ', true, 5);
+INSERT INTO parts ( Name, Requared, Count) values( 'процессор', true, 2);
+INSERT INTO parts ( Name, Requared, Count) values( 'подсветка корпуса', false, 10);
+INSERT INTO parts ( Name, Requared, Count) values( 'звуковая карта', true, 4);
+INSERT INTO parts ( Name, Requared, Count) values( 'кард ридер', false, 4);
+INSERT INTO parts ( Name, Requared, Count) values( 'корпус ', true, 2);
+INSERT INTO parts ( Name, Requared, Count) values('Видеокарты', true, 2);
+INSERT INTO parts ( Name, Requared, Count) values('Оптические приводы', true, 2);
+INSERT INTO parts ( Name, Requared, Count) values( 'ТВ-тюнеры и видеомонтаж', true, 0);
+INSERT INTO parts ( Name, Requared, Count) values( 'Аксессуары для устройств охлаждения', true, 2);
+INSERT INTO parts ( Name, Requared, Count) values( 'Термопаста ', true, 5);
+INSERT INTO parts ( Name, Requared, Count) values( 'Сетевые адаптеры', true, 2);
+INSERT INTO parts ( Name, Requared, Count) values( 'подсветка корпуса', false, 10);
+INSERT INTO parts ( Name, Requared, Count) values( 'звуковая карта', true, 4);
+INSERT INTO parts ( Name, Requared, Count) values( 'кард ридер', false, 4);
+INSERT INTO parts ( Name, Requared, Count) values( 'корпус ', true, 2);
+
+
+
+
